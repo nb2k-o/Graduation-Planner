432fd31d5a51762aa232ff0141f207c4d5cae027
miirae <miira.efrem@gmail.com>
2023-12-16 19:58:45 -0500

    landing page connected except opening and creating plan functionalities

---
+++ backend/templates/landingpage.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+    sessionStorage.setItem("currentUser", "ooo2139@columbia.edu");
+    let currentUser = sessionStorage.getItem("currentUser");
+    console.log(currentUser);
+    //let currentUser = sessionStorage.getItem("currentUser");
+    /**if (currentUser == null) {
+        console.log("undefined");
+        window.location.href = "landingpage.html"; //will change to login once that gets in static pages
+    }
+    console.log(currentUser);**/
+
+    async function getPopularPlans(){
+        let url =
+        "http://127.0.0.1:5000/popular_plans"
+
+        // Storing response
+        const response = await fetch(url);
+
+        // Storing data in form of JSON
+        var data = await response.json();
+        console.log(data);
+        if (response) {
+            displayPopularPlans(data.data.plans);
+        }
+        else {
+            document.getElementById("grid-container").innerHTML = `<span>Error</span>`;
+        }
+    }
+
+    console.log(getPopularPlans());
+
+
+    function displayPopularPlans(data){
+        let plans = ''
+        for (let i = 0; i < 4; i++) {
+            plans += `<div class="grid-item"> 
+            <div id="header"> 
+                <div id="top">
+                    <span class= "name">${data[i].title}</span>
+                    <span class= "author">${data[i].author_name}</span>
+                </div>
+                
+                <div>
+                    <label class= "tags">${data[i].tags}</label>
+                </div>
+                <br>
+                <div>
+                    <label class= "likes"><img src="../static/heart.png" class="planicon">${data[i].likes}</label>
+                    <label class= "comments"><img src="../static/comment.png" class="planicon">${data[i].comments}</label>
+                </div>
+            </div>
+        </div>`;
+        }
+        document.getElementById("grid-container").innerHTML = plans;
+    }
+
+    
+    $("#suggested-button").click(function () {
+        window.location.href = "suggestedplans.html";
+    })
+
+    $("#profile-button").click(function () {
+        window.location.href = "profile.html";
+    })
+
+    $("#search-b").click(function () {
+        let currentSearch = sessionStorage.setItem("currentSearch", $(".search-bar").val());
+        window.location.href = "search.html";
+    })
+
+
+    
+    getPopularPlans();
+    
+
+})
