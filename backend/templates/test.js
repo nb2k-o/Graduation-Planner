5b46d901bcfafd34e9e982be23d63965b21986e8
nb2k.o <bnigel.2002@yahoo.com>
2023-12-19 04:55:33 -0500

    Done! :smile:

--- backend/templates/test.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    let currentUser = sessionStorage.setItem("currentUser", "ooo2139@columbia.edu");
-    console.log(currentUser);
-    window.location.href = "profile.html";
-})
